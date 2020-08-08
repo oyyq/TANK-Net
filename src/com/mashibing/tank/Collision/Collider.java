a2a6a0b6be15a58ec23bbf019890a17887ef918f
ouyangyunq <iwtknowm@gmail.com>
2020-08-08 13:45:48 -0700

    测试修改后再次提交git

--- src/com/mashibing/tank/Collision/Collider.java
+++ src/com/mashibing/tank/Collision/Collider.java
@@ -1,11 +1,10 @@
 package com.mashibing.tank.Collision;
 
-
-
 import com.mashibing.tank.GameObject.GameObject;
 
 import java.io.Serializable;
 
 public interface Collider extends Serializable {
     boolean collide(GameObject o1, GameObject o2);
+
 }
