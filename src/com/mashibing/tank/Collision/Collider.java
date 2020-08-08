0f23af91d51cc7631a763ea811a916b555e5fe79
ouyangyunq <iwtknowm@gmail.com>
2020-08-08 14:07:28 -0700

    第三次修改

--- src/com/mashibing/tank/Collision/Collider.java
+++ src/com/mashibing/tank/Collision/Collider.java
@@ -5,6 +5,12 @@ import com.mashibing.tank.GameObject.GameObject;
 import java.io.Serializable;
 
 public interface Collider extends Serializable {
+
+
+
+
+
+
     boolean collide(GameObject o1, GameObject o2);
 
 }
